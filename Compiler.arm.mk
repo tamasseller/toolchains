6bbc0cb45a3b45b72be1f5c2c295bcd01fb3ca15
Dudas Zoltan <zedudi@gmail.com>
2018-10-06 15:56:41 +0000

    Update complier version reference to 7.3.1

--- Compiler.arm.mk
+++ Compiler.arm.mk
@@ -1,4 +1,4 @@
-LIB_DIRS += /usr/lib/gcc/arm-none-eabi/6.3.1/thumb/v6-m/
+LIB_DIRS += /usr/lib/gcc/arm-none-eabi/7.3.1/thumb/v6-m/
 LIB_DIRS += /usr/lib/arm-none-eabi/lib/thumb/v6-m/
 
 GCCPREF = arm-none-eabi-
